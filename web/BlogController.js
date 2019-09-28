ff5ae21c892576131dd382a464f4471a2f998301
hkp <sxhkp163@163.com>
2019-09-28 20:46:03 +0800

    博客文章编辑

--- web/BlogController.js
+++ web/BlogController.js
@@ -1,4 +1,6 @@
 var blogDao = require('../dao/BlogDao');
+var tagsDao = require('../dao/TagsDao');
+var tagBlogMappingDao = require('../dao/TagBlogMappingDao');
 var timeUtil = require('../util/timeUtil');
 var respUtil = require('../util/respUtil');
 var url = require('url');
@@ -9,7 +11,7 @@ function editBlog(request,response) {
     var tags = params.tags.replace(/ /g, '').replace('，',',');
     request.on('data',function (data) {
         blogDao.insertBlog(params.title,data.toString(),0,tags,timeUtil.getNow(),timeUtil.getNow(),function (result) {
-            response.write(200);
+            response.writeHead(200);
             response.write(respUtil.writeResult('success','添加成功',null));
             response.end();
             
@@ -24,7 +26,30 @@ function editBlog(request,response) {
         });
     })
 }
+path.set('/editBlog',editBlog);
 
-function queryTag() {
-    
+// 查询tag
+function queryTag(tag,blogId) {
+    tagsDao.queryTag(tag,function(result){
+        if(result == null || result.length == 0){
+            insertTag(tag,blogId);
+            // 如果没有标签直接插入标签，并且插入映射
+        }else{
+            // 如果有标签
+            tagBlogMappingDao.insertTagBlogMapping(result[0].id,blogId,timeUtil.getNow(),timeUtil.getNow());
+        }
+    })
 }
+
+function insertTag(tag,blogId){
+    tagsDao.insertTag(tag,timeUtil.getNow(),timeUtil.getNow(),function(result){
+        insertTagBlogMapping(result.insertId,blogId);
+    })
+}
+
+function insertTagBlogMapping(tagId,blogId){
+    tagBlogMappingDao.insertTagBlogMapping(tagId,blogId,timeUtil.getNow(),timeUtil.getNow(),function(result){
+    })
+}
+
+module.exports.path = path;
